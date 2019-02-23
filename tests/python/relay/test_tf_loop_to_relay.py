45b74a49b483009bec11d4213e16f70b6d714b42
Jared Roesch <roeschinc@gmail.com>
2019-02-23 01:21:31 -0800

    Add first example of rewriting

---
+++ tests/python/relay/test_tf_loop_to_relay.py
@@ -0,0 +1,23 @@
+import tensorflow as tf
+import numpy as np
+from tvm import relay
+from tvm.relay.frontend.tensorflow import from_tensorflow
+
+def ex1():
+    i = tf.constant(0)
+    c = lambda i: tf.less(i,10)
+    b = lambda i: tf.add(i,1)
+    r = tf.while_loop(c, b, [i])
+
+    graph = tf.get_default_graph()
+
+    with tf.Session() as sess:
+        tf_out = sess.run(r)
+
+    expr, params = from_tensorflow(graph.as_graph_def(add_shapes=True))
+    ex = relay.create_executor('debug')
+    relay_out = ex.evaluate(expr)(**params)
+    np.testing.assert_allclose(tf_out, relay_out.asnumpy())
+
+
+ex1()
