e96e631a22fac4e82da87ac09bd9afce870031a0
Jared Roesch <roeschinc@gmail.com>
2019-02-23 01:29:29 -0800

    Add second test

--- tests/python/relay/test_tf_loop_to_relay.py
+++ tests/python/relay/test_tf_loop_to_relay.py
@@ -4,6 +4,7 @@ from tvm import relay
 from tvm.relay.frontend.tensorflow import from_tensorflow
 
 def ex1():
+    tf.reset_default_graph()
     i = tf.constant(0)
     c = lambda i: tf.less(i,10)
     b = lambda i: tf.add(i,1)
@@ -20,4 +21,24 @@ def ex1():
     np.testing.assert_allclose(tf_out, relay_out.asnumpy())
 
 
-ex1()
+def ex2():
+    tf.reset_default_graph()
+    i0 = tf.constant(0)
+    j0 = tf.ones([2,2])
+    c = lambda i, j: i < 10
+    b = lambda i, j: [tf.add(i,1), j]
+    r = tf.while_loop(c, b, loop_vars=[i0, j0])
+
+    graph = tf.get_default_graph()
+
+    with tf.Session() as sess:
+        tf_out = sess.run(r)
+
+    expr, params = from_tensorflow(graph.as_graph_def(add_shapes=True))
+    ex = relay.create_executor('debug')
+    relay_out = ex.evaluate(expr)(**params)
+    np.testing.assert_allclose(tf_out, relay_out.asnumpy())
+
+if __name__ == "__main__":
+    ex1()
+    ex2()
