fa4a6a366cba388afb71e3a74ebc945ecda4088c
Haichen Shen <shenhaichen@gmail.com>
2019-04-15 11:55:19 -0700

    Fix bug

--- src/relay/pass/type_solver.cc
+++ src/relay/pass/type_solver.cc
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -500,7 +500,7 @@ Type TypeSolver::Unify(const Type& dst, const Type& src, const NodeRef& node, bo
   Type ret = unifier.Unify(dst, src, arg_type);
   if (!ret.defined()) {
     LOG(FATAL) << "failed to unify " << dst << " <-> " << src << " at "
-               << RelayPrint(node, false);
+               << AsText(node, false);
   }
   return ret;
 }
