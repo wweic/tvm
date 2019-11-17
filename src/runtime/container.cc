f25e6a5ccf7c9c0ab9ab15d81b29d02f9aa85a20
Wei Chen <ipondering.weic@gmail.com>
2019-11-16 20:02:51 -0800

    Update

--- src/runtime/container.cc
+++ src/runtime/container.cc
@@ -39,7 +39,7 @@ ObjectRef ADTObj::operator[](size_t idx) const {
 void* ADTObj::AddressOf(int i) const {
   ADTObj* self = const_cast<ADTObj*>(this);
   char* fields = reinterpret_cast<char*>(self) + sizeof(ADTObj);
-  ObjectRef* field_p = reinterpret_cast<ObjectRef>(fields + i * sizeof(ObjectRef));
+  ObjectRef* field_p = reinterpret_cast<ObjectRef*>(fields + i * sizeof(ObjectRef));
   std::cout << (void*)field_p << " count: " << field_p->use_count() << "\n";
   return field_p;
 }
